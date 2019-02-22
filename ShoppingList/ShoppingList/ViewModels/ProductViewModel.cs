f80141fab2e1c1c32343e014ca578ca81c76648b
Sullivan Lacheteau <lacheteau.s@gmail.com>
2019-02-22 23:42:30 +0100

    Product cell UI + CheckBox renderer

--- ShoppingList/ShoppingList/ViewModels/ProductViewModel.cs
+++ ShoppingList/ShoppingList/ViewModels/ProductViewModel.cs
@@ -9,9 +9,21 @@ namespace ShoppingList.ViewModels
     {
 		public string Name { get; set; }
 
+		public int Quantity { get; set; }
+
+		public double Price { get; set; }
+
+		public string QuantityAndName => $"{Quantity} {Name}";
+
+		public string TotalPrice => Price == 0 ? "-" : $"{((Quantity > 1) ? Price * Quantity : Price)}€";
+
+		public string UnitPrice => (Price > 0 && Quantity > 1) ? $"({Price}€ / unit)" : "";
+
 		public ProductViewModel(ProductModel product)
 		{
 			Name = product.Name;
+			Quantity = product.Quantity;
+			Price = product.Price;
 		}
     }
 }
