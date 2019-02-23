3bdce94c2527ec1c5a1641a80a2170a708bae652
Sullivan Lacheteau <lacheteau.s@gmail.com>
2019-02-24 00:02:18 +0100

    Added NavigationService

---
+++ ShoppingList/ShoppingList/Services/NavigationService/INavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Services
+{
+	public interface INavigationService
+	{
+		void Map<TViewModel, TView>();
+		Task NavigateToAsync<TViewModel>(bool isModal = false);
+	}
+}
---
+++ ShoppingList/ShoppingList/Services/NavigationService/NavigationService.cs
@@ -0,0 +1,34 @@
+﻿using ShoppingList.Core;
+using ShoppingList.ViewModels;
+using ShoppingList.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShoppingList.Services
+{
+	public class NavigationService : INavigationService
+	{
+		INavigation _navigation => Application.Current.MainPage.Navigation;
+
+		private static readonly Dictionary<string, Type> _mapping = new Dictionary<string, Type>();
+
+		public void Map<TViewModel, TView>()
+		{
+			_mapping.Add(nameof(TViewModel), typeof(TView));
+		}
+
+		public Task NavigateToAsync<TViewModel>(bool isModal = false)
+		{
+			var viewType = _mapping[nameof(TViewModel)];
+			var viewInstance = (Page)IoC.GetInstance(viewType);
+
+			if (isModal)
+				return _navigation.PushModalAsync(viewInstance);
+			else
+				return _navigation.PushAsync(viewInstance);
+		}
+	}
+}
